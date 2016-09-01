21f10d9132e95055e29e599949592a1034515ca7
Iftekhar Tanveer <itanveer@f15.cs.rochester.edu>
2016-09-01 11:19:42 -0400

    Changed the sample_usage file

--- sample_usage.py
+++ sample_usage.py
@@ -52,24 +52,24 @@ if __name__=='__main__':
     cls.test_avg_corr(show_plots=True,tot_iter=1000,task = 'classification',method='lda',equalize_MT_sample_size=True)
     # cls.test_avg_corr(show_plots=True,tot_iter=1000,method='max-margin')
     # ----------- Regression with MLP ---------
-    # cls = Classify_MLP('./all_features_MT_gt.pkl')
-    # cls.test_avg_corr()
-    # cls = Classify_MLP('./all_features_gt.pkl')
-    # cls.test_avg_corr()
-    # # ------------ Classification -------------
-    # cls = Classify('all_features_MT_gt.pkl')
-    # cls.test_avg_corr(show_plots=False,method='max-margin',task='classification',tot_iter=10)
-    # # Use the participants' self annotations
-    # cls = Classify('all_features_gt.pkl')
-    # cls.test_avg_corr(show_plots=True,method='max-margin',task='classification',tot_iter=10)    
-    # # ----------- Classification with MLP ---------
-    # cls = Classify_MLP('./all_features_MT_gt.pkl')
-    # cls.test_avg_corr(task='classification')
-    # cls = Classify_MLP('./all_features_gt.pkl')
-    # cls.test_avg_corr(task='classification')
-    # # ----------- Classification with LASSO ---------
-    # cls = Classify_MLP('./all_features_MT_gt.pkl')
-    # cls.test_avg_corr(show_all=True,task='classification',method='LASSO')
-    # cls = Classify_MLP('./all_features_gt.pkl')
-    # cls.test_avg_corr(show_all=True,task='classification',method='LASSO')
+    cls = Classify_MLP('./all_features_MT_gt.pkl')
+    cls.test_avg_corr()
+    cls = Classify_MLP('./all_features_gt.pkl')
+    cls.test_avg_corr()
+    # ------------ Classification -------------
+    cls = Classify('all_features_MT_gt.pkl')
+    cls.test_avg_corr(show_plots=False,method='max-margin',task='classification',tot_iter=10)
+    # Use the participants' self annotations
+    cls = Classify('all_features_gt.pkl')
+    cls.test_avg_corr(show_plots=True,method='max-margin',task='classification',tot_iter=10)    
+    # ----------- Classification with MLP ---------
+    cls = Classify_MLP('./all_features_MT_gt.pkl')
+    cls.test_avg_corr(task='classification')
+    cls = Classify_MLP('./all_features_gt.pkl')
+    cls.test_avg_corr(task='classification')
+    # ----------- Classification with LASSO ---------
+    cls = Classify_MLP('./all_features_MT_gt.pkl')
+    cls.test_avg_corr(show_all=True,task='classification',method='LASSO')
+    cls = Classify_MLP('./all_features_gt.pkl')
+    cls.test_avg_corr(show_all=True,task='classification',method='LASSO')
 
